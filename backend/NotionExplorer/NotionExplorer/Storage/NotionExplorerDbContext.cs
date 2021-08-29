17a0fbc7796b8e7299dabb6bca2b306749194fb4
Marcel Schot <marcel.schot@gmail.com>
2021-08-29 12:16:49 +0200

    Add entity framework

---
+++ backend/NotionExplorer/NotionExplorer/Storage/NotionExplorerDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using DbContext = Microsoft.EntityFrameworkCore.DbContext;
+
+namespace NotionExplorer.Storage
+{
+    [DbConfigurationType(typeof(StorageConfiguration))]
+    public class NotionExplorerDbContext : DbContext
+    {
+    }
+}
